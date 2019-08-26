aa0ad9b5db3fda999cfc69ef9764bc62cf0ca895
sagar <sagar.sahay@spektrix.com>
2019-08-26 16:48:40 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CsvParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csv = @"Year,Title,Production Studio,
+                        2008,Iron Man,Marvel Studios,";
+            var sr = new StringReader(csv);
+            var csvReader = new CsvReader(sr);
+            foreach (var line in csvReader.Lines)
+                Console.WriteLine(line.First(p => p.Key == "Title").Value);
+        }
+    }
+}
